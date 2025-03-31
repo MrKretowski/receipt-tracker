10e9f6c49dba714fc28f76ec5e7d5b4ebbbe44e4
Szymon Kretowski <S.P.Kretowski@student.tudelft.nl>
2025-03-31 12:17:37 +0200

    day page front 34

--- src/pages/day/[day].js
+++ src/pages/day/[day].js
@@ -368,7 +368,7 @@ const styles = {
   // Container with two buttons side by side
   plusMinusContainer: {
     position: "absolute",
-    left: "50%",
+    left: "12rem",
     top: "50%",
     transform: "translate(-50%, -50%)",
     display: "flex",
